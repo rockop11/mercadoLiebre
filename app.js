6f14722a7dc8bc1f83f2cfc7a58e48370c8fcd44
Rockop11 <rockoponce1422@gmail.com>
2021-01-31 00:40:08 -0300

    ruteo ok de login y register

--- app.js
+++ app.js
@@ -6,18 +6,25 @@ const app = express();
 
 const publicPath = path.resolve('public');
 
-app.use(express.static(publicPath))
+app.use(express.static(publicPath));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get ('/', function(req, res) {
+app.get('/', function(req, res) {
     res.sendFile(path.resolve('./views/home.html'));
 });
 
-app.get ('/register.html', function(req, res) {
+app.get('/register.html', function(req, res) {
     res.sendFile(path.resolve('./views/register.html'));
 });
+app.post('/register', function(req, res) {
+    res.send(req.body);
+});
 
-app.get ('/login.html', function(req, res) {
+app.get('/login.html', function(req, res) {
     res.sendFile(path.resolve('./views/login.html'));
+});
+app.post('/login', function(req, res) {
+    res.send(req.body);
 });
 
 app.listen(3000, function(){
